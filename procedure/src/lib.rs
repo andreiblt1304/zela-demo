3b59c7a5dfa5e53f4f4054ac331bf235990ef4d0
Andrei Baltariu <andreibaltariu1304@gmail.com>
2026-02-15 10:33:42 +0200

    Test commit

--- procedure/src/lib.rs
+++ procedure/src/lib.rs
@@ -38,7 +38,7 @@ pub struct ProcedureErrorData {
 }
 
 impl CustomProcedure for LeaderRoutingProcedure {
-    type Params = [JsonValue; 1];
+    type Params = JsonValue;
     type SuccessData = LeaderRoutingOutput;
     type ErrorData = ProcedureErrorData;
 
